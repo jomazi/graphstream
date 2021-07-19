a1b9a049ec9df44bda822d9de2578436acc671e4
jomazi <>
2021-07-19 15:17:47 +0200

    adjust app for quick testing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
-use graphstream::print_hello_world;
+use graphstream as gs;
 
 fn main() {
-    print_hello_world();
+    gs::print_hello_world();
+    println!("{}", gs::util::Error::Internal("test error".to_string()));
+    let graph_stream = gs::GraphStream::new("co-occurrence", false);
+    println!(
+        "New graph stream with edge types {}.",
+        graph_stream.edge_type
+    )
 }
