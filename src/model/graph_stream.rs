b22eab06e0a58736e6a769c78ac67bf470e4c0fe
jomazi <>
2021-07-20 12:54:35 +0200

    add length method

--- src/model/graph_stream.rs
+++ src/model/graph_stream.rs
@@ -4,7 +4,7 @@ use std::collections::BTreeMap;
 // generic type means that node identifier can be of different type, e.g. numeric ID, string literal
 
 pub struct GraphStream<'a, T> {
-    edge_stream: BTreeMap::<i64, [T; 2]>, // key is UNIX time; see https://docs.rs/chrono/0.4.19/chrono/struct.DateTime.html#method.timestamp (accessed 19-7-21)
+    edge_stream: BTreeMap<i64, [T; 2]>, // key is UNIX time; see https://docs.rs/chrono/0.4.19/chrono/struct.DateTime.html#method.timestamp (accessed 19-7-21)
     pub edge_type: &'a str,
     pub directed: bool, // whether or not edges are directed
 }
@@ -19,5 +19,8 @@ impl<'a, T> GraphStream<'a, T> {
     }
     pub fn insert(&mut self, timestamp: i64, node_1: T, node_2: T) {
         self.edge_stream.insert(timestamp, [node_1, node_2]);
+    }
+    pub fn len(&self) -> usize {
+        self.edge_stream.len()
     }
 }
