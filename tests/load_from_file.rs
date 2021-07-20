dcfaf5c85d41953d1172383beaf68283d236bd6d
jomazi <>
2021-07-20 12:54:44 +0200

    add first test

---
+++ tests/load_from_file.rs
@@ -0,0 +1,9 @@
+extern crate graphstream as gs;
+
+#[test]
+fn load_from_file() {
+    let graph_stream =
+        gs::load_from_file::<_, u8>("./tests/test-stream.csv", ",", true, "co-occurrence", false)
+            .unwrap();
+    assert_eq!(graph_stream.len(), 3)
+}
